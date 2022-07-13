dd5b7b89d29de66d77f0087cbfe44cee1974721d
Roman <barabanov1233@gmail.com>
2022-07-13 20:47:27 +0400

    button docs

--- storybook/svelte-typescript-app/src/stories/Button/Button.stories.svelte
+++ storybook/svelte-typescript-app/src/stories/Button/Button.stories.svelte
@@ -1,11 +1,17 @@
 <script>
     import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
     import { Button, EButtonVariant, EColor } from '@yllo/ui-lib'
+    import docs from './docs.mdx'
 </script>
 
 <Meta
     title="Button"
     component={Button}
+    parameters={{
+        docs: {
+            page: docs,
+        },
+    }}
     argTypes={{
         // Props example
         isRounded: {
@@ -63,12 +69,27 @@
     <Button {...args} on:click={args.onClick}>Button</Button>
 </Template>
 
-<Story name="Outlined" args={{ variant: EButtonVariant.outlined, color: EColor.line7 }} />
+<Story name="Default" args={{ variant: EButtonVariant.outlined, color: EColor.line7 }} />
 
-<Story name="Outlined 2" args={{ variant: EButtonVariant.outlined2, color: EColor.line7 }} />
+<Story name="Variants">
+    <div class="row">
+        <Button variant={EButtonVariant.outlined} color={EColor.line7}>Outlined</Button>
+        <Button variant={EButtonVariant.outlined2} color={EColor.line7}>Outlined 2</Button>
+        <Button variant={EButtonVariant.filled} color={EColor.line1}>Filled</Button>
+        <Button variant={EButtonVariant.text} color={EColor.line4}>Text</Button>
+        <Button variant={EButtonVariant.text2} color={EColor.line5}>Text 2</Button>
+    </div>
+</Story>
 
-<Story name="Filled" args={{ variant: EButtonVariant.filled, color: EColor.line1 }} />
-
-<Story name="Text" args={{ variant: EButtonVariant.text, color: EColor.line4 }} />
-
-<Story name="Text 2" args={{ variant: EButtonVariant.text2, color: EColor.line5 }} />
+<style>
+    .row {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+    }
+    .row :global(.button) {
+        margin: 20px;
+    }
+</style>
